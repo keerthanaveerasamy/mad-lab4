d96aea8e7939961a18bf5d3b95898ea36120a6ac
ITLAB <ITLAB@IT206>
2017-02-20 14:29:35 +0530

    commit5

--- src/com/example/ex4/FileOperations.java
+++ src/com/example/ex4/FileOperations.java
@@ -3,18 +3,14 @@ package com.example.ex4;
 public class FileOperations {
 
 	public boolean write(String filename, String filecontent) {
-		// TODO Auto-generated method stub
 		return false;
+		// TODO Auto-generated method stub
+		
 	}
 
 	public String read(String readfilename) {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	public void write(String filename, String filecontent) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
